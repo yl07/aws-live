029476ff9725c1d6fa76ab12c397cd5532134733
yl07 <70637096+yl07@users.noreply.github.com>
2022-09-01 14:47:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ct8hgo3am5jz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liewyanlin-employee"
+customregion = "us-east-1"
